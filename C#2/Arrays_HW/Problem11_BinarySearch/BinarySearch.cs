687c7fef436e66b24a41f42bd71a0031fb4706a5
Daniela Popova <d.i.popova@abv.bg>
2016-05-12 20:53:23 +0300

    add hw C#2 Arrays

---
+++ C#2/Arrays_HW/Problem11_BinarySearch/BinarySearch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class BinarySearch
+{
+    static void Main()
+    {
+        //string[] input = Console.ReadLine().Split(' ');
+
+        //int[] nums = new int[input.Length];
+
+        //for (int i = 0; i < input.Length; i++)
+        //{
+        //    nums[i] = int.Parse(input[i]);
+        //    //Console.WriteLine(nums[i]);
+        //}
+
+        int len = int.Parse(Console.ReadLine());
+        int[] nums = new int[len];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(nums);
+
+        int searchedNum = int.Parse(Console.ReadLine());
+
+        int start = 0;
+        int end = nums.Length - 1;
+        int mid = 0;
+
+        bool found = false;
+
+        while (!found && start <= end)
+        {
+            mid = ((start + end) / 2);
+
+            if (searchedNum == nums[mid])
+            {
+                found = true;
+            }
+            else
+	        {
+                if (searchedNum > nums[mid])
+                {
+                    start = mid + 1;
+                }
+                else if (searchedNum < nums[mid])
+                {
+                    end = mid - 1;
+                }
+	        }
+        }
+
+        if (found == true)
+        {
+            Console.WriteLine(mid);
+        }
+        else
+        {
+            Console.WriteLine(-1);
+        }
+    }
+}
+
---
+++ C#2/Arrays_HW/Problem15_PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class PrimeNumbers
+{
+    static void Main()
+    {
+        //60/100 bgcoder - timeLimit
+        int num = int.Parse(Console.ReadLine());
+
+        int[] array = new int[num + 1];
+        int maxNum = int.MinValue;
+
+        int count = 2;
+        for (int i = 2; i <= num; i++)
+        {
+            array[i] = count;
+            count++;            
+        }
+
+        for (int i = 2; i * i <= num; i++)
+        {
+            for (int j = (i * i); j <= num; j = j + i)
+            {
+                array[j] = 0;
+            }
+        }
+        Array.Sort(array);
+        maxNum = array[array.Length - 1];    
+               
+        Console.WriteLine(maxNum);
+    }
+}
+
