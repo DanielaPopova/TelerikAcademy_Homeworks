a801ff97f51e0c3c1e31cbd9c44493cce6e48351
Daniela Popova <d.i.popova@abv.bg>
2016-06-21 13:15:51 +0300

    add hw oop principles part 1/ school

---
+++ C#OOP/OOPPrinciples1_HW/01.School/SchoolModel/School.cs
@@ -0,0 +1,60 @@
+﻿namespace Schools.SchoolModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;   
+
+    public class School
+    {
+        private List<SchoolClass> classes;
+
+        public School()
+        {
+            this.Classes = new List<SchoolClass>();
+        }
+
+        public List<SchoolClass> Classes
+        {
+            get
+            {
+                return this.classes;
+            }
+
+            private set
+            {
+                this.classes = value;
+            }
+        }
+
+        public void AddClass(SchoolClass someClass)
+        {
+            this.Classes.Add(someClass);
+        }
+
+        public void RemoveClass(SchoolClass someClass)
+        {
+            if (this.Classes.Count == 0)
+            {
+                throw new ArgumentException("There are no classes!");
+            }
+            else
+            {
+                this.Classes.Remove(someClass);
+                SchoolClass.uniqueIds.Remove(someClass.Id);
+            }
+        }
+
+        public string PrintSchoolInfo()
+        {
+            StringBuilder schoolInfo = new StringBuilder();
+
+            schoolInfo.Append("School information:\n");
+            foreach (var someClass in this.Classes)
+            {
+                schoolInfo.Append(someClass.PrintClassInfo() + "\n");
+            }
+
+            return schoolInfo.ToString();
+        }
+    }
+}
---
+++ C#OOP/OOPPrinciples1_HW/01.School/SchoolModel/SchoolClass.cs
@@ -0,0 +1,132 @@
+﻿namespace Schools.SchoolModel
+{    
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+    using Schools.People;
+
+    public class SchoolClass
+    {
+        private string id;
+        private List<Student> students;
+        private List<Teacher> teachers;
+        internal static List<string> uniqueIds = new List<string>();
+
+        public SchoolClass(string id)
+        {
+            this.Id = id;
+            this.Students = new List<Student>();
+            this.Teachers = new List<Teacher>();
+        }
+
+        public List<Student> Students
+        {
+            get
+            {
+                return this.students;
+            }
+
+            private set
+            {
+                this.students = value;
+            }
+        }
+
+        public List<Teacher> Teachers
+        {
+            get
+            {
+                return this.teachers;
+            }
+
+            private set
+            {
+                this.teachers = value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return this.id;
+            }
+
+            private set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("Class Id mst have more than 1 symbols");
+                }
+
+                if (uniqueIds.Contains(value))
+                {
+                    throw new ArgumentException("There is already a class with that ID!");
+                }
+                else
+                {
+                    uniqueIds.Add(value);
+                }
+
+                this.id = value;
+            }
+        }
+
+        public void AddStudent(Student student)
+        {
+            this.Students.Add(student);
+        }
+
+        public void Addteacher(Teacher teacher)
+        {
+            this.Teachers.Add(teacher);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            if (this.Students.Count == 0)
+            {
+                throw new ArgumentException("There are no students!");
+            }
+            else
+            {
+                this.Students.Remove(student);
+                Student.uniqueClassNumber.Remove(student.ClassNumber);
+            }
+        }
+
+        public void RemoveTeacher(Teacher teacher)
+        {
+            if (this.Teachers.Count == 0)
+            {
+                throw new ArgumentException("there are no teachers!");
+            }
+            else
+            {
+                this.Teachers.Remove(teacher);
+            }
+        }
+
+        public string PrintClassInfo()
+        {
+            StringBuilder classInfo = new StringBuilder();
+
+            classInfo.Append(this.Id + " class\n");
+            classInfo.Append("Teachers:\n");
+            foreach (var teacher in this.Teachers)
+            {
+                classInfo.Append(teacher.Name + "\n" + teacher.PrintDisciplinesInfo());
+            }
+
+            classInfo.Append("Students:\n");
+
+            foreach (var student in this.Students)
+            {
+                classInfo.Append(student.Name + " Number: " + student.ClassNumber + "\n");
+            }
+
+            return classInfo.ToString();
+        }       
+    }
+}
