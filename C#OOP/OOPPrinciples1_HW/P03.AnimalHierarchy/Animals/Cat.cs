7cc92660825e9acecefba982b255fc48f95f3e5c
Daniela Popova <d.i.popova@abv.bg>
2016-06-23 10:12:23 +0300

    added abstract class

--- C#OOP/OOPPrinciples1_HW/P03.AnimalHierarchy/Animals/Cat.cs
+++ C#OOP/OOPPrinciples1_HW/P03.AnimalHierarchy/Animals/Cat.cs
@@ -3,7 +3,7 @@
     using AnimalHierarchy.Enumerations;
     using AnimalHierarchy.Interfaces;
 
-    public class Cat : Animal
+    public abstract class Cat : Animal
     {
         public Cat(string name, double age, Gender gender) : base(name, age, gender)
         {
