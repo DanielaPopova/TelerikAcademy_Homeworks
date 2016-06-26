a472387498e8dde150e7201192f6024de449f861
Daniela Popova <d.i.popova@abv.bg>
2016-06-26 22:52:14 +0300

    edit 7 sorting array

--- JS Fundamentals/06.Functions/07.SortingArray.js
+++ JS Fundamentals/06.Functions/07.SortingArray.js
@@ -1,17 +1,16 @@
 function sortArray(args) {
-  var sortedArray = [],
-      numbers = args[0].replace('\n', ' ').split(' ').map(Number);
-      numbers.shift();
+  var len = +args[0],
+      numbers = args[1].split(' ').map(Number);
 
   return sortByDescendingOrder(numbers).reverse().join(' ');
-  
+
   function sortByDescendingOrder(array) {
     var i, len, maxEl, maxElIndex, temp;
 
     for (i = 0, len = array.length; i < len; i += 1) {
-      maxEl = getMax(i, array);      
-      maxElIndex = array.indexOf(maxEl, i);      
-      temp = array[i];      
+      maxEl = getMax(i, array);
+      maxElIndex = array.indexOf(maxEl, i);
+      temp = array[i];
       array[i] = maxEl;
       array[maxElIndex] = temp;
     }
@@ -20,7 +19,7 @@ function sortArray(args) {
 
   function getMax(index, numArray) {
     var i, len,
-        maxElement = Number.NEGATIVE_INFINITY;
+      maxElement = Number.NEGATIVE_INFINITY;
 
     for (i = index, len = numArray.length; i < len; i += 1) {
       if (numArray[i] > maxElement) {
@@ -35,9 +34,9 @@ function sortArray(args) {
   // });
 }
 
-console.log(sortArray(['6\n3 4 1 5 2 6']));
-console.log(sortArray(['10\n36 10 1 34 28 38 31 27 30 20']));
-console.log(sortArray(['4\n2 -2 2 0']));  //if maxElement = Number.MIN_VALUE - first number is not 0 but 5e-324
+console.log(sortArray(['6', '3 4 1 5 2 6']));
+console.log(sortArray(['10', '36 10 1 34 28 38 31 27 30 20']));
+console.log(sortArray(['4', '2 -2 2 0']));  //if maxElement = Number.MIN_VALUE - first number is not 0 but 5e-324
 /*
 Write a method that returns the maximal element in a portion of array of integers starting at given index.
 Using it write another method that sorts an array in ascending / descending order. Write a program that sorts a given array.
