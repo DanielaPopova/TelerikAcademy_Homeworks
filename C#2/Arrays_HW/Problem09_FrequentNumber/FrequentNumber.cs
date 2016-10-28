17ab25ec3528bcb9632cbd02e40dde1a0cccd997
Daniela Popova <d.i.popova@abv.bg>
2016-10-28 16:13:05 +0300

    Update FrequentNumber.cs
    added another solution to problem

--- C#2/Arrays_HW/Problem09_FrequentNumber/FrequentNumber.cs
+++ C#2/Arrays_HW/Problem09_FrequentNumber/FrequentNumber.cs
@@ -52,3 +52,57 @@ class FrequentNumber
     }
 }
 
+//Different aproach
+
+//namespace Homeworks
+//{
+//    using System;    
+
+//    class Program
+//    {
+//        static void Main()
+//        {            
+//            int length = int.Parse(Console.ReadLine());
+//            int[] array = new int[length];          
+
+//            for (int i = 0; i < length; i++)
+//            {
+//               array[i] = int.Parse(Console.ReadLine());
+//            }
+
+//            int maxCount = 1;
+//            int currCount = 1;
+//            int mostFrequentNumber = array[0];
+
+//            for (int i = 0; i < array.Length - 1; i++)
+//            {
+//                int currElement = array[i];
+
+//                for (int j = i + 1; j < array.Length; j++)
+//                {
+//
+//                    if (array[j] == int.MinValue)
+//                    {
+//                        continue;
+//                    }
+//                    else if (currElement == array[j])
+//                    {
+//                        currCount++;
+
+//                        if (currCount > maxCount)
+//                        {
+//                            maxCount = currCount;
+//                            mostFrequentNumber = currElement;
+//                        }
+
+//                        array[j] = int.MinValue;
+//                    }
+//                }
+//                currCount = 1;
+//            }
+
+//            Console.WriteLine("{0} ({1} times)", mostFrequentNumber, maxCount);
+//        }
+//    }
+//}
+
