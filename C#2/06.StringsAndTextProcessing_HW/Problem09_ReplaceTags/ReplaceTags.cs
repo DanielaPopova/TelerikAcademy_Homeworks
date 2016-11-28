11a6071b498aa0c64debd3b0b22c092d92a2cb28
Daniela Popova <d.i.popova@abv.bg>
2016-11-28 16:20:52 +0200

    added solution for 09.ReplaceTags

--- C#2/06.StringsAndTextProcessing_HW/Problem09_ReplaceTags/ReplaceTags.cs
+++ C#2/06.StringsAndTextProcessing_HW/Problem09_ReplaceTags/ReplaceTags.cs
@@ -3,65 +3,41 @@ using System.Text;
 
 class ReplaceTags
 {
-    static void Main()                 //time limit/bgcoder
+    static void Main()
     {
-        string input = Console.ReadLine(); //"<p>Please visit <a href=\"http://academy.telerik.com\">our site</a> to choose a training course. Also visit <a href=\"www.devbg.org\">our forum</a> to discuss the courses.</p>";
-
-        int startIndex = input.IndexOf("<a");
-        int endIndex = input.IndexOf("</a>") + 4;
-        string toEdit = String.Empty;
-
+        string text = Console.ReadLine();
         StringBuilder result = new StringBuilder();
+        
+        string openTag = "<a href=";
+        string endTag = "</a>";
 
-        for (int i = 0; i < input.Length; i++)
+        int startIndexOpenTag = text.IndexOf(openTag);
+        int indexCloseTag = text.IndexOf(endTag);
+        int indexCurrPart = 0;
+
+        while (startIndexOpenTag != -1 || indexCloseTag != -1)
         {
-            while (startIndex > 0)
+            string currPart = text.Substring(indexCurrPart, startIndexOpenTag - indexCurrPart);            
+            string toEdit = text.Substring(startIndexOpenTag, indexCloseTag - startIndexOpenTag + endTag.Length);            
+            string url = "(" + toEdit.Substring(openTag.Length + 1, (toEdit.IndexOf(">") - openTag.Length - 1 - 1)) + ")";
+            string innerText = "[" + toEdit.Substring(toEdit.IndexOf(">") + 1, toEdit.LastIndexOf("<") - toEdit.IndexOf(">") - 1) + "]";
+
+            result.Append(currPart);   //result.Replace(toEdit, innerText + url); //timelimit for every test
+            result.Append(innerText);
+            result.Append(url);
+            
+            indexCurrPart = indexCloseTag + endTag.Length;            
+            startIndexOpenTag = text.IndexOf(openTag, startIndexOpenTag + 1);
+            indexCloseTag = text.IndexOf(endTag, indexCloseTag + 1);
+
+            if (startIndexOpenTag == -1 && indexCloseTag == -1)
             {
-                toEdit = input.Substring(startIndex, endIndex - startIndex);    //<a href=\"http://academy.telerik.com\">our site</a>
-
-                int startURL = toEdit.IndexOf("\"") + 1;
-                int endURL = toEdit.LastIndexOf("\"");
-                string url = toEdit.Substring(startURL, endURL - startURL);    //http://academy.telerik.com
-                //Console.WriteLine(url);
-
-                int startText = toEdit.IndexOf(">") + 1;
-                int endText = toEdit.IndexOf("</a>");
-                string text = toEdit.Substring(startText, endText - startText);    //our site
-                //Console.WriteLine(text);
-
-                string edited = String.Format("[{0}]({1})", text, url);
-
-                input = input.Replace(toEdit, edited);
-                //Console.WriteLine(result);
-
-                startIndex = input.IndexOf("<a", startIndex + 1);
-                endIndex = input.IndexOf("</a>", endIndex) + 4;
-
-                //Console.WriteLine(toEdit);
+                currPart = text.Substring(indexCurrPart);
+                result.Append(currPart);
             }
         }
 
-        Console.WriteLine(input);
+        Console.WriteLine(result);
     }
 }
-
-/*
-Write a program that replaces in a HTML document given as string all the tags <a href="URL">TEXT</a> with corresponding markdown notation [TEXT](URL).
-
-Input
-
-On the only input line you will receive a string
-Output
-
-Print the string with replaced tags
-Constraints
-
-Time limit: 0.1s
-Memory limit: 16MB
-Sample tests
-
-Input	                                                                                        Output
-<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course.  <p>Please visit [our site](http://academy.telerik.com) to choose a training course. 
-Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>	                Also visit [our forum](www.devbg.org) to discuss the courses.</p>
-*/
 
