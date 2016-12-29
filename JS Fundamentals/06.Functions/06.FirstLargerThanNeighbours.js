624bd33635883ce363e08be750f3eb3f40ad238b
Daniela Popova <d.i.popova@abv.bg>
2016-12-29 20:15:04 +0200

    updated js hw Functions

--- JS Fundamentals/06.Functions/06.FirstLargerThanNeighbours.js
+++ JS Fundamentals/06.Functions/06.FirstLargerThanNeighbours.js
@@ -1,22 +1,23 @@
 function printIndex(args) {
-  
+
     var numbers = args[1].split(' ').map(Number);
-    
+
     return findIndex(numbers);
 
-  function findIndex(numArray) {
-    var i, len, index;
-    for (i = 1, len = numArray.length; i < len - 1; i += 1) {
-      if (numArray[i] > numArray[i - 1] && numArray[i] > numArray[i + 1]) {
-        index = i;
-        break;
-      }
+    function findIndex(numArray) {
+        var i, len, index = -1;
+        for (i = 1, len = numArray.length; i < len - 1; i += 1) {
+            if (numArray[i] > numArray[i - 1] && numArray[i] > numArray[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
-    return index;
-  }
 }
 
 console.log(printIndex(['6', '-26 -25 -28 31 2 27']));
+console.log(printIndex(['5', '1 2 3 4 5']));  // check if no such element exists -1
 
 /*
 Write a method that returns the index of the first element in array that is larger than its neighbours, or -1, if there is no such element.
