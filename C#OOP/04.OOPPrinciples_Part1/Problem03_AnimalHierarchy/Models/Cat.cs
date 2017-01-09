1a2a128fb181939177d033dc5636542c7ca96e15
Daniela Popova <d.i.popova@abv.bg>
2017-01-09 22:22:20 +0200

    added new c#OOP hw OOPPrinciples Part 1

---
+++ C#OOP/04.OOPPrinciples_Part1/Problem03_AnimalHierarchy/Models/Cat.cs
@@ -0,0 +1,12 @@
+﻿namespace AnimalHierarchy.Models
+{
+    using AnimalHierarchy.Enumerations;
+
+    public abstract class Cat : Animal
+    {
+        public Cat(string name, double age, GenderType gender)
+            : base(name, age, gender)
+        {
+        }
+    }
+}
