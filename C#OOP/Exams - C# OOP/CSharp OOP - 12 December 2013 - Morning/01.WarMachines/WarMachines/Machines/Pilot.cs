eda44f6c60233d8b6e222f3f3f8d8498516e2f5f
Daniela Popova <d.i.popova@abv.bg>
2017-01-15 15:14:23 +0200

    update

---
+++ C#OOP/Exams - C# OOP/CSharp OOP - 12 December 2013 - Morning/01.WarMachines/WarMachines/Machines/Pilot.cs
@@ -0,0 +1,61 @@
+﻿namespace WarMachines.Machines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using WarMachines.Common;
+    using WarMachines.Interfaces;
+
+    public class Pilot : IPilot
+    {
+        private string name;
+        private ICollection<IMachine> machines;
+
+        public Pilot(string name)
+        {
+            this.Name = name;
+            this.machines = new List<IMachine>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                Validator.CheckIfStringIsNullOrEmpty(value, GlobalErrorMessages.StringCannotBeNullOrEmpty);
+
+                this.name = value;
+            }
+        }
+
+        public void AddMachine(IMachine machine)
+        {
+            Validator.CheckIfNull(machine, GlobalErrorMessages.ObjectCannotBeNull);
+
+            this.machines.Add(machine);
+        }
+
+        public string Report()
+        {
+            var sortedMachines = this.machines
+                                .OrderBy(m => m.HealthPoints)
+                                .ThenBy(m => m.Name);
+
+            var result = new StringBuilder();
+            result.AppendLine(string.Format("{0} - {1} {2}", this.Name, this.machines.Count > 0 ? this.machines.Count.ToString() : "no", this.machines.Count == 1 ? "machine" : "machines"));
+
+            foreach (var machine in sortedMachines)
+            {
+                result.AppendLine(machine.ToString());
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ C#OOP/Exams - C# OOP/CSharp OOP - 12 December 2013 - Morning/01.WarMachines/WarMachines/WarMachinesProgram.cs
@@ -0,0 +1,25 @@
+﻿namespace WarMachines
+{
+    using WarMachines.Engine;
+    using WarMachines.Machines;
+
+    public class WarMachinesProgram
+    {
+        public static void Main()
+        {
+            WarMachineEngine.Instance.Start();
+
+            // testing return this.targets/new List<string>(this.targets)
+            //var machine = new Tank("T12", 20, 50);
+            //machine.Attack("blue");
+            //machine.Attack("yellow");
+            //machine.Targets.Add("pink");
+            //foreach (var target in machine.Targets)
+            //{
+            //    System.Console.WriteLine(target);
+            //}
+            //output from return this.targets -> blue, yellow, pink
+            //output from return new List<targets> - > blue, yellowe 
+        }
+    }
+}
