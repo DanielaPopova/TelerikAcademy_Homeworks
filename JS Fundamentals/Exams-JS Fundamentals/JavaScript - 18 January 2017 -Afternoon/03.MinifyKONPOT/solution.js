f8bd620dce46645b3aefb8a98659c329a4e20706
Daniela Popova <d.i.popova@abv.bg>
2017-01-23 00:27:27 +0200

    todo: Minify Konpot

--- JS Fundamentals/Exams-JS Fundamentals/JavaScript - 18 January 2017 -Afternoon/03.MinifyKONPOT/solution.js
+++ JS Fundamentals/Exams-JS Fundamentals/JavaScript - 18 January 2017 -Afternoon/03.MinifyKONPOT/solution.js
@@ -1,23 +1,12 @@
 /*jshint esversion: 6 */
 function solve(params) {
     let allInOne = params.join().replace(/\s+/g, ''),
-        count = 0,
-        countStars = 0;        
+        count = 0;               
     const patternFirstSymbol = /[a-zA-Z0-9_]+/g;
 
-    let newAllInOne = allInOne.replace(patternFirstSymbol, '*');
-
-    for (let i = 0, len = newAllInOne.length; i < len; i += 1) {
-        let currSymbol = newAllInOne[i];
-
-        if (currSymbol === '*') {
-            countStars += 1;
-        }        
-    }
+    //TODO:
     
-    count = countStars * 2;
     
-    console.log(count);
 }
 
 solve([
