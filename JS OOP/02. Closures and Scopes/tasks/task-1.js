e9ab0779021bc18ce3d678b35f74d51256acd90a
Daniela Popova <d.i.popova@abv.bg>
2017-01-27 00:14:30 +0200

    some formatting

--- JS OOP/02. Closures and Scopes/tasks/task-1.js
+++ JS OOP/02. Closures and Scopes/tasks/task-1.js
@@ -6,62 +6,62 @@ function solve() {
 
         function listBooks(optional) {
 
-        		let result = [];
+            let result = [];
 
-        		if (!optional) {
-        				result = books;
-        		} else if(optional && optional.category) {
-        				result = books.filter(function (book) {
-        						return book.category === optional.category;
-        				});
-        		} else if(optional && optional.author) {
-        				result = books.filter(function (book) {
-        					  return book.author === optional.author;
-        				});
-        		}
+            if (!optional) {
+                result = books;
+            } else if (optional && optional.category) {
+                result = books.filter(function(book) {
+                    return book.category === optional.category;
+                });
+            } else if (optional && optional.author) {
+                result = books.filter(function(book) {
+                    return book.author === optional.author;
+                });
+            }
 
-        		result = result.sort((firstBook, secondBook) => {
-        				return firstBook.ID - secondBook.ID;
-        		});
+            result = result.sort((firstBook, secondBook) => {
+                return firstBook.ID - secondBook.ID;
+            });
 
             return result;
         }
 
         function addBook(book) {
 
-        		//validate title
-        		if (!book.title || book.title.length < 2 || book.title.length > 100) {
-        				throw new Error('Book title must be between 2 and 100 characters!');
-        		}
-        		
-        		if (isTitleUnique(book)) {
-        				throw new Error('This title already exists!');
-        		}
+            //validate title
+            if (!book.title || book.title.length < 2 || book.title.length > 100) {
+                throw new Error('Book title must be between 2 and 100 characters!');
+            }
 
-        		//validate author
-        		if (!book.author) {
-        				throw new Error('Author must not be a non-empty string!');
-        		}
+            if (isTitleUnique(book)) {
+                throw new Error('This title already exists!');
+            }
 
-        		//validate isbn
-        		if (!book.isbn || book.isbn.length !== 10 && book.isbn.length !== 13) {
-        				throw new Error('Book ISBN must contain either 10 or 13 digits!');
-        		}
+            //validate author
+            if (!book.author) {
+                throw new Error('Author must not be a non-empty string!');
+            }
 
-        		if (!isNumber(book.isbn)) {
-        				throw new Error('Book ISBN must be a valid number!');
-        		}
-        		
-        		if (isIsbnUnique(book)) {
-        				throw new Error('This isbn already exists!');
-        		}
+            //validate isbn
+            if (!book.isbn || book.isbn.length !== 10 && book.isbn.length !== 13) {
+                throw new Error('Book ISBN must contain either 10 or 13 digits!');
+            }
 
-        		//vaidate category
-        		if (categories.indexOf(book.category) === -1) {
+            if (!isNumber(book.isbn)) {
+                throw new Error('Book ISBN must be a valid number!');
+            }
+
+            if (isIsbnUnique(book)) {
+                throw new Error('This isbn already exists!');
+            }
+
+            //vaidate category
+            if (categories.indexOf(book.category) === -1) {
                 categories.push(book.category);
             }
 
-        		book.ID = books.length + 1;
+            book.ID = books.length + 1;
             books.push(book);
 
             return book;
@@ -87,21 +87,22 @@ function solve() {
         }
 
         function isTitleUnique(someBook) {
-        		
-        		return books.some(b => {
-        				return b.title === someBook.title;
-        		});
+
+            return books.some(b => {
+                return b.title === someBook.title;
+            });
         }
 
         function isIsbnUnique(someBook) {
-        		
-        		return books.some(b => {
-        				return b.isbn === someBook.isbn;
-        		});
+
+            return books.some(b => {
+                return b.isbn === someBook.isbn;
+            });
         }
 
     }());
 
     return library;
 }
+
 module.exports = solve;
