2886d4aad3dc31808554cd48eb485ca2b855d5a0
Daniela Popova <d.i.popova@abv.bg>
2017-01-30 19:48:37 +0200

    added validation for Infinity

--- JS OOP/01. Functions and Functions Expressions/tasks/task-1.js
+++ JS OOP/01. Functions and Functions Expressions/tasks/task-1.js
@@ -1,39 +1,39 @@
-function solve() {
+// function solve() {
 
-    return function sumNumbers(numbers) {
+//     return function sumNumbers(numbers) {
 
-        if (numbers === undefined) {
-            throw new Error('You should pass an argument!');
-        }
+//         if (numbers === undefined) {
+//             throw new Error('You should pass an argument!');
+//         }
 
-        if (!numbers.length) {
-            return null;
-        }
+//         if (!numbers.length) {
+//             return null;
+//         }
 
-        let sum = 0;
+//         let sum = 0;
 
-        for (let i = 0, len = numbers.length; i < len; i += 1) {
-            let currEl = numbers[i];
+//         for (let i = 0, len = numbers.length; i < len; i += 1) {
+//             let currEl = +numbers[i];
 
-            if (isNaN(currEl)) {
-                throw new Error('All elements in the array should be numbers!');
-            }
+//             if (isNaN(currEl) || !isFinite(currEl)) {
+//                 throw new Error('All elements in the array should be numbers!');
+//             }
 
-            sum += Number(currEl);
-        }
+//             sum += Number(currEl);
+//         }
 
-        return sum;
-    };
-
-}
+//         return sum;
+//     };
+// }
 
 module.exports = solve;
 
-console.log(solve()([1, 2, 3]));
-console.log(solve()([]));
-console.log(solve()([1, 'john', 3]));
-console.log(solve()([1, 2, 3]));
-console.log(solve()([1, '2', 3]));
-console.log(solve()([1, '2.23', 3]));
-console.log(solve()([1.77, '2.23', 3]));
+// console.log(solve()([]));
+// console.log(solve()());
+//console.log(solve()([Infinity, Infinity]));
+//console.log(solve()([1, 'john', 3]));
+// console.log(solve()([1, 2, 3]));
+// console.log(solve()([1, '2', 3]));
+// console.log(solve()([1, '2.23', 3]));
+// console.log(solve()([1.77, '2.23', 3]));
 
