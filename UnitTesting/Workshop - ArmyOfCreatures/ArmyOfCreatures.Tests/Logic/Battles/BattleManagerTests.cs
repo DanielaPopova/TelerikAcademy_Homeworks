046641ce5fe19d5acf433fffd2ac938db8d841da
Daniela Popova <d.i.popova@abv.bg>
2017-02-14 22:38:24 +0200

    added tests for Battles/Specialties/Commands

---
+++ UnitTesting/Workshop - ArmyOfCreatures/ArmyOfCreatures.Tests/Logic/Battles/BattleManagerTests.cs
@@ -0,0 +1,93 @@
+﻿namespace ArmyOfCreatures.Tests.Logic.Battles
+{
+    using System;
+
+    using NUnit.Framework;
+    using Moq;
+    using ArmyOfCreatures.Logic;
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Logic.Creatures;
+
+    [TestFixture]
+    public class BattleManagerTests
+    {
+        //Add creatures should throw ArgumentNullException, when Identifier is null
+        [Test]
+        public void AddCreatures_IdentifierIsNull_ShouldThrowArgumentNullException()
+        {
+            //Arrange
+            CreatureIdentifier creatureID = null;
+            var factoryMock = new Mock<ICreaturesFactory>();
+            var loggerMock = new Mock<ILogger>();
+
+            var battleManager = new BattleManager(factoryMock.Object, loggerMock.Object);
+
+            //Act/Assert
+            Assert.Throws<ArgumentNullException>(() => battleManager.AddCreatures(creatureID, 3));
+        }
+
+        //Add creatures should call CreteCreature from factory
+        [Test]
+        public void AddCreatures_ShouldCallCreateCreatureFromFactory()
+        {
+            //Arrange
+            var creatureID = CreatureIdentifier.CreatureIdentifierFromString("Angel(1)");
+            var creature = new Angel();
+            var factoryMock = new Mock<ICreaturesFactory>();
+            var loggerMock = new Mock<ILogger>();
+            var battleManager = new BattleManager(factoryMock.Object, loggerMock.Object);
+
+            factoryMock.Setup(f => f.CreateCreature(It.IsAny<string>())).Returns(creature);
+
+            //Act
+            battleManager.AddCreatures(creatureID, 2);
+
+            //Assert
+            factoryMock.Verify(f => f.CreateCreature(It.IsAny<string>()), Times.Once);
+        }
+
+        //Add creatures should call WriteLine from Logger
+        [Test]
+        public void AddCreatures_ShoulCallWriteLineFromLogger()
+        {
+            //Arrange
+            var creatureID = CreatureIdentifier.CreatureIdentifierFromString("Angel(1)");
+            var creature = new Angel();
+            var factoryMock = new Mock<ICreaturesFactory>();
+            var loggerMock = new Mock<ILogger>();
+            var battleManager = new BattleManager(factoryMock.Object, loggerMock.Object);
+
+            factoryMock.Setup(f => f.CreateCreature(It.IsAny<string>())).Returns(creature);
+            loggerMock.Setup(l => l.WriteLine(It.IsAny<string>()));
+
+            //Act
+            battleManager.AddCreatures(creatureID, 2);
+
+            //Assert
+            loggerMock.Verify(l => l.WriteLine(It.IsAny<string>()), Times.Once);
+        }
+
+        //Adding creature to Army 3 (not existing) should throw ArgumentException
+        [Test]
+        public void AddCreatures_AddingToArmy3_ShouldThrowArgumentException()
+        {
+            //Arrange
+            int armyNumber = 3;
+            var creatureID = CreatureIdentifier.CreatureIdentifierFromString("Angel(" + armyNumber + ")");
+            var creature = new Angel();
+            var factoryMock = new Mock<ICreaturesFactory>();
+            var loggerMock = new Mock<ILogger>();
+            var battleManager = new BattleManager(factoryMock.Object, loggerMock.Object);
+
+            factoryMock.Setup(f => f.CreateCreature("Angel")).Returns(creature);
+
+            //Act/Assert
+            Assert.Throws<ArgumentException>(() => battleManager.AddCreatures(creatureID, 2));
+
+            var er = Assert.Throws<ArgumentException>(() => battleManager.AddCreatures(creatureID, 2));
+            StringAssert.Contains("Invalid ArmyNumber", er.Message);
+
+            Assert.That(() => battleManager.AddCreatures(creatureID, 2), Throws.ArgumentException.With.Message.Contains("Invalid ArmyNumber"));
+        }
+    }
+}
---
+++ UnitTesting/Workshop - ArmyOfCreatures/ArmyOfCreatures.Tests/Logic/Specialties/DoubleDefenseWhenDefendingTests.cs
@@ -0,0 +1,74 @@
+﻿namespace ArmyOfCreatures.Tests.Logic.Specialties
+{
+    using System;
+
+    using NUnit.Framework;
+    using Moq;
+    using ArmyOfCreatures.Logic.Specialties;
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Logic.Creatures;
+
+    [TestFixture]
+    public class DoubleDefenseWhenDefendingTests
+    {
+        [Test]
+        public void ApplyWhenDefending_DefenderWithSpecialtyIsNull_ShouldThrowArgumentNullExceotion()
+        {
+            //Arrange
+            var defense = new DoubleDefenseWhenDefending(3);
+            ICreaturesInBattle nullDefender = null;
+            var attackerMock = new Mock<ICreaturesInBattle>();
+
+            //Act/Assert
+            Assert.Throws<ArgumentNullException>(() => defense.ApplyWhenDefending(nullDefender, attackerMock.Object));
+        }
+
+        [Test]
+        public void ApplyWhenDefending_AttackerIsNull_ShouldThrowArgumentNullExceotion()
+        {
+            //Arrange
+            var defense = new DoubleDefenseWhenDefending(3);
+            ICreaturesInBattle nullAttacker = null;
+            var defenderMock = new Mock<ICreaturesInBattle>();
+
+            //Act/Assert
+            Assert.Throws<ArgumentNullException>(() => defense.ApplyWhenDefending(defenderMock.Object, nullAttacker));
+        }
+
+        //[Test]
+        //public void ApplyWhenDefending_EffectIsExpired_ShouldNotChangeCurrentDefenceOfDefenderWithSpecialty()
+        //{
+        //    //Arrange
+        //    int rounds = 1;
+        //    var defense = new DoubleDefenseWhenDefending(rounds);
+
+        //    var defenderWithSpecialtyMock = new Mock<ICreaturesInBattle>();
+        //    var attackerMock = new Mock<ICreaturesInBattle>();
+
+        //    defenderWithSpecialtyMock.SetupGet(d => d.CurrentDefense).Returns(100);
+
+        //    //Act/Assert
+        //    defense.ApplyWhenDefending(defenderWithSpecialtyMock.Object, attackerMock.Object);
+            
+        //}
+
+        [Test]
+        public void ApplyWhenDefending_EffectIsNotExpired_ShouldDoubleCurrentDefenceOfDefenderWithSpecialty()
+        {
+            //Arrange
+            int rounds = 1;
+            var defense = new DoubleDefenseWhenDefending(rounds);
+
+            var creature = new Angel();
+            var defenderWithSpecialty = new CreaturesInBattle(creature, 5);
+            defenderWithSpecialty.CurrentDefense = 30;
+            var attackerMock = new Mock<ICreaturesInBattle>();          
+
+            //Act
+            defense.ApplyWhenDefending(defenderWithSpecialty, attackerMock.Object);
+
+            //Assert
+            Assert.AreEqual(60, defenderWithSpecialty.CurrentDefense);
+        }
+    }
+}
