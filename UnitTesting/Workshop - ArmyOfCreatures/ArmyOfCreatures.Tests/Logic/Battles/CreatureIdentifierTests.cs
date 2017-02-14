dfab6d1780423639d66df8fe6530c7035eaa06a6
Daniela Popova <d.i.popova@abv.bg>
2017-02-14 23:15:38 +0200

    added tests to CreatureIdentifiers

---
+++ UnitTesting/Workshop - ArmyOfCreatures/ArmyOfCreatures.Tests/Logic/Battles/CreatureIdentifierTests.cs
@@ -0,0 +1,63 @@
+﻿namespace ArmyOfCreatures.Tests.Logic.Battles
+{
+    using System;
+
+    using NUnit.Framework;
+    using ArmyOfCreatures.Logic.Battles;
+
+    [TestFixture]
+    public class CreatureIdentifierTests
+    {
+        //Call with null valueToParse should throw ArgumentNullException
+        [Test]
+        public void CreatureIdentifier_ValueToParseIsNull_ShouldThrowArgumentNullException()
+        {
+            //Arrange
+            string nullValueToParse = null;
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => CreatureIdentifier.CreatureIdentifierFromString(nullValueToParse));
+            Assert.That(() => CreatureIdentifier.CreatureIdentifierFromString(nullValueToParse), Throws.ArgumentNullException.With.Message.Contains("valueToParse"));
+        }
+
+        //Call with invalid Army number (ex: Angel(test)) test cannot be parsed should throw FormatExcepition
+        [Test]
+        public void CreatureIdentifier_ValueToParseHasInvalidArmyNumber_ShouldThrowFormatException()
+        {
+            //Arrange --correct format exm Angel(1)--
+            string invalidArmyNumberInValueToParse = "Angel(invalid)";
+
+            //Act/Assert
+            Assert.Throws<FormatException>(() => CreatureIdentifier.CreatureIdentifierFromString(invalidArmyNumberInValueToParse));
+            Assert.That(() => CreatureIdentifier.CreatureIdentifierFromString(invalidArmyNumberInValueToParse),
+               Throws.InstanceOf<FormatException>());
+        }
+
+        //Call with invalid string (without brackets to split) should throw IndexOutOfRangeException
+        [Test]
+        public void CreatureIdentifier_ValueToParseHasnoBracketsToBeSplittedBy_ShouldThrowIndexOutOfRangeException()
+        {
+            //Arrange
+            string valueToParseWithoutBrackets = "Angel";
+
+            //Act/Assert
+            Assert.Throws<IndexOutOfRangeException>(() => CreatureIdentifier.CreatureIdentifierFromString(valueToParseWithoutBrackets));
+        }
+
+        //ToString should output expected result
+        [Test]
+        public void ToString_ValidInput_ShouldReturnExpectedoutput()
+        {
+            //Arrange
+            string expected = "Angel(1)";
+            
+            var creatureId = CreatureIdentifier.CreatureIdentifierFromString("Angel(1)");
+
+            //Act
+            var actual = creatureId.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
