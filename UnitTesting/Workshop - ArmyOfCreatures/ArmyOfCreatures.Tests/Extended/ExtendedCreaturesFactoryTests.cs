92a557d51b7568a3cf6460ea429e8b783bc1bf57
Daniela Popova <d.i.popova@abv.bg>
2017-02-15 00:30:27 +0200

    added tests for ExtendedCreaturesFactory

---
+++ UnitTesting/Workshop - ArmyOfCreatures/ArmyOfCreatures.Tests/Extended/ExtendedCreaturesFactoryTests.cs
@@ -0,0 +1,46 @@
+﻿namespace ArmyOfCreatures.Tests.Extended
+{
+    using System;
+
+    using NUnit.Framework;
+    using ArmyOfCreatures.Extended;
+    using ArmyOfCreatures.Extended.Creatures;
+    using ArmyOfCreatures.Logic.Creatures;
+
+    [TestFixture]
+    public class ExtendedCreaturesFactoryTests
+    {
+        /*CreateCreature should throw ArgumentException with message that contains the string "Invalid creature type",
+         when a string representing non-existing creature type is passed as a method argument.*/
+        [Test]
+        public void CreateCreature_PassedTypeIsNonExisting_ShouldThrowArgumentExceptionWithExpectedMessage()
+        {
+            //Arrange
+            string invalidType = "invalid";
+            var factory = new ExtendedCreaturesFactory();
+
+            //Act/Assert
+            Assert.That(() => factory.CreateCreature(invalidType), Throws.ArgumentException.With.Message.Contain("Invalid creature type"));
+        }
+
+        /*CreateCreature should return the corresponding creature type based on the string that is passed as a method argument.
+         Test with (AncientBehemoth, CyclopsKing, Goblin, Griffin, WolfRaider, and something else for the default case).*/
+        [TestCase("AncientBehemoth", typeof(AncientBehemoth))]
+        [TestCase("CyclopsKing", typeof(CyclopsKing))]
+        [TestCase("Goblin", typeof(Goblin))]
+        [TestCase("Griffin", typeof(Griffin))]
+        [TestCase("WolfRaider", typeof(WolfRaider))]
+        [TestCase("Angel", typeof(Angel))]
+        public void CreateCreature_PassedTypeIsValid_ShouldReturnCorrespondingCreatureType(string stringType, Type expectedType)
+        {
+            //Arrange
+            var factory = new ExtendedCreaturesFactory();
+
+            //Act
+            var actualType = factory.CreateCreature(stringType).GetType();
+
+            //Assert
+            Assert.AreEqual(expectedType, actualType);
+        }
+    }
+}
