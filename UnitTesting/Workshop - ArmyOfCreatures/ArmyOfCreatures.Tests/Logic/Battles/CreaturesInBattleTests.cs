5556f38a18587dbb3a39cecbfd4e30521161a6fe
Daniela Popova <d.i.popova@abv.bg>
2017-02-15 01:00:20 +0200

    added some more tests to workshop armyofcreatures

---
+++ UnitTesting/Workshop - ArmyOfCreatures/ArmyOfCreatures.Tests/Logic/Battles/CreaturesInBattleTests.cs
@@ -0,0 +1,30 @@
+﻿namespace ArmyOfCreatures.Tests.Logic.Battles
+{
+    using System;
+
+    using NUnit.Framework;
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Logic.Creatures;
+
+    [TestFixture]
+    public class CreaturesInBattleTests
+    {
+        public object ICreatureInBattle { get; private set; }
+
+        //Deal Damage with null defender should throw ArgumentNullException
+        [Test]
+        public void DealDamage_DefenderIsNull_ShouldThrowArgumentNullException()
+        {
+            //Arrange
+            ICreaturesInBattle defender = null;
+            var creature = new Angel();
+
+            var sut = new CreaturesInBattle(creature, 2);
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => sut.DealDamage(defender));
+        }
+
+        //Deal Damage should return expected result        
+    }
+}
