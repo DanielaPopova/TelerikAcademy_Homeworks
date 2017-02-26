f6073ac2827b1cd7188c0cccbaa8a4c0c7bd39ba
Daniela Popova <d.i.popova@abv.bg>
2017-02-26 21:09:20 +0200

    finished project with PerformOperation

--- UnitTesting/Exams-UnitTesting/UnitTestingExam - 16 february 2017/AcademyPackageManager/PackageManager.Tests/Core/PackageInstallerTests.cs
+++ UnitTesting/Exams-UnitTesting/UnitTestingExam - 16 february 2017/AcademyPackageManager/PackageManager.Tests/Core/PackageInstallerTests.cs
@@ -16,6 +16,7 @@
     public class PackageInstallerTests
     {
         //Test for restoring packages when the object is constructed
+        //should test when there are dependencies in the project
         [Test]
         public void Constructor_WhenObjectIsConstructed_ShouldRestorePackages()
         {
@@ -34,7 +35,51 @@
             projectMock.Verify(p => p.PackageRepository.GetAll(), Times.Once);
         }
 
-        //should test when there are dependencies in the project
-        
+        //PerformOperation - Test for Install command and empty dependencies list - should call two times Download and one time Remove
+        [Test]
+        public void PerformOperation_InstallCommandWithEmptyDependencies_ShouldCall2TimesDownload1TimeRemove()
+        {
+            //Arrange
+            var downloaderMock = new Mock<IDownloader>();
+            var projectMock = new Mock<IProject>();
+            var packageMock = new Mock<IPackage>();
+            packageMock.SetupGet(p => p.Dependencies).Returns(new List<IPackage>());
+            projectMock.Setup(p => p.PackageRepository.GetAll()).Returns(new List<IPackage>());
+
+            var installer = new PackageInstallerMock(downloaderMock.Object, projectMock.Object);
+            installer.Operation = InstallerOperation.Install;
+
+            //Act
+            installer.PerformOperation(packageMock.Object);
+
+            //Assert
+            downloaderMock.Verify(d => d.Remove(It.IsAny<string>()), Times.Once);
+            downloaderMock.Verify(d => d.Download(It.IsAny<string>()), Times.Exactly(2));
+        }
+
+        //PerformOperation - Test for Install command and at least one dependency in the dependencies list
+        //- every dependency on the chain will multiply the calss to the Download and Remove mehtod by 2
+        [Test]
+        public void PerformOperation_InstallCommandWithOneDependency_ShouldCall4TimesDownload2TimeRemove()
+        {
+            //Arrange
+            var downloaderMock = new Mock<IDownloader>();
+            var projectMock = new Mock<IProject>();
+            var packageMock = new Mock<IPackage>();
+            var packageDependencyMock = new Mock<IPackage>();
+
+            packageDependencyMock.SetupGet(p => p.Dependencies).Returns(new List<IPackage>());
+            packageMock.SetupGet(p => p.Dependencies).Returns(new List<IPackage>() { packageDependencyMock.Object});
+            projectMock.Setup(p => p.PackageRepository.GetAll()).Returns(new List<IPackage>());
+
+            var installer = new PackageInstallerMock(downloaderMock.Object, projectMock.Object);
+
+            //Act
+            installer.PerformOperation(packageMock.Object);
+
+            //Assert
+            downloaderMock.Verify(d => d.Remove(It.IsAny<string>()), Times.Exactly(2));
+            downloaderMock.Verify(d => d.Download(It.IsAny<string>()), Times.Exactly(4));
+        }
     }
 }
