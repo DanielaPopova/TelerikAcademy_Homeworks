f1a70ce43ae2d1cd8ec1293b9230b38d47365edc
Daniela Popova <d.i.popova@abv.bg>
2017-03-18 21:59:15 +0200

    added ControlFlowConditionalStatementsLoops hw

---
+++ High Quality Code Part 1/05. Control Flow, Conditional Statements and Loops/ControlFlowConditionalStatementsLoops/Problem01_ClassCheff/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace Kitchen
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Kitchen.Contracts;
+    using Kitchen.Models;
+    using Kitchen.Models.Products;
+    using Kitchen.Models.Utensils;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var vegetables = new List<IVegetable>()
+            {
+                new Potato(),
+                new Carrot(),
+                new Potato(),
+                new Carrot(),
+                new Carrot()
+            };
+
+            var bowl = new Bowl();
+            var chef = new Chef();
+            chef.Cook(vegetables, bowl);
+        }
+    }
+}
---
+++ High Quality Code Part 1/05. Control Flow, Conditional Statements and Loops/ControlFlowConditionalStatementsLoops/Problem03_RefactorLoop/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace RefactorLoop
+{
+    using System;
+    
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int size = 20;
+            int expectedValue = 5;
+            bool isFound = false;
+            int[] numbers = new int[size];
+
+            FillArrayWithRandomNumbers(numbers, 10);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+
+                if (i % 10 == 0 && numbers[i] == expectedValue)
+                {
+                    isFound = true;
+                    // Console.WriteLine("Index - {0}", i);
+                    break;
+                }
+            }
+
+            if (isFound)
+            {
+                Console.WriteLine("Value found!");
+            }           
+        }
+
+        public static void FillArrayWithRandomNumbers(int[] array, int maxRange)
+        {
+            var randomGen = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomGen.Next(0, maxRange);
+            }
+        }
+    }
+}
