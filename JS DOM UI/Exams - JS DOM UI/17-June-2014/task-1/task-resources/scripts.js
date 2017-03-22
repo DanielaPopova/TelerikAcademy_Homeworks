55b59c24dfd621a1c4eea2715b3570b27ddef95d
Daniela Popova <d.i.popova@abv.bg>
2017-03-22 20:15:13 +0200

    added solution for task 1

--- JS DOM UI/Exams - JS DOM UI/17-June-2014/task-1/task-resources/scripts.js
+++ JS DOM UI/Exams - JS DOM UI/17-June-2014/task-1/task-resources/scripts.js
@@ -1,73 +1,117 @@
 function createImagesPreviewer(selector, items) {
-  var root = document.querySelector(selector);
-  root.className += ' image-previewer';
-  
-  var table = document.createElement('table');
-  table.style.height = '400px';
-  table.style.overflow = 'hidden';
-  
-  var theRow = document.createElement('tr');
-  table.appendChild(theRow);
-  root.appendChild(table);
-  
-  /* Left panel */
-  var leftPanel = document.createElement('td');  
-  leftPanel.className += ' left-panel';
+
+    var container = document.querySelector(selector);
     
-  var title = document.createElement('h1');
-  title.innerHTML = items[0].title;
-  
-  var bigImage = document.createElement('img');
-  bigImage.src = items[0].url;
-  
-  leftPanel.appendChild(title);
-  leftPanel.appendChild(bigImage);    
-  /* End of left panel */    
-  
-  /* Right panel */
-  var rightPanel = document.createElement('td');
-  rightPanel.className += ' right-panel';
-  rightPanel.style.textAlign = 'center';
-  rightPanel.style.overflowY = 'scroll';
+    //Left Panel
+    var leftPanel = document.createElement('section');
+    leftPanel.className = 'image-preview';    
+    leftPanel.style.display = 'inline-block';
+    leftPanel.style.float = 'left';
 
-  var filter = document.createElement('div');
-  filter.className = 'filter';
-  var filterTitle = document.createElement('span');
-  filterTitle.innerHTML = 'Filter';
-  var filterBox = document.createElement('input');
-  filterBox.type = 'text';
-  filter.appendChild(filterTitle);
-  filter.appendChild(document.createElement('br'));
-  filter.appendChild(filterBox);
+    var leftPanelTitle = document.createElement('h1');
+    leftPanelTitle.innerHTML = items[0].title;
+    leftPanelTitle.style.textAlign = 'center';
 
-  rightPanel.appendChild(filter);
+    var leftPanelImage = document.createElement('img');
+    leftPanelImage.setAttribute('src', items[0].url);
+    leftPanelImage.setAttribute('alt', items[0].title);
+    leftPanelImage.style.width = '400px';
+    leftPanelImage.style.height = '400px';
+    leftPanelImage.style.borderRadius = '10px';
 
-  var imgsList = document.createElement('ul');
-  imgsList.style.listStyleType = 'none';
-  imgsList.style.height = '200px';
-  for (var i = 0; i < items.length; i += 1) {
+    leftPanel.appendChild(leftPanelTitle);
+    leftPanel.appendChild(leftPanelImage);
     
-    var imgItem = document.createElement('li');
-    imgItem.setAttribute('data-index', i + '');
-    imgItem.style.width = '150px';
+    //Right Panel
+    var rightPanel = document.createElement('aside');
+    rightPanel.style.display = 'inline-block';
+    rightPanel.style.height = '500px';
+    rightPanel.style.margin = '10px';
+    rightPanel.style.padding = '10px';
+    rightPanel.style.overflowY = 'scroll';
 
-    var imgItemTitle = document.createElement('span');
-    imgItemTitle.innerHTML = items[i].title;
-    imgItem.appendChild(imgItemTitle);
+    var filterBox = document.createElement('input');
+    filterBox.type = 'text';
+    var filterBoxTitle = document.createElement('h4');
+    filterBoxTitle.innerHTML = 'Filter';
+    filterBoxTitle.style.textAlign = 'center';
+    filterBoxTitle.style.margin = 0;
 
-    var imgItemImg = document.createElement('img');
-    imgItemImg.src = items[i].url;
-    imgItemImg.style.maxWidth = '100%';
+    var imageContainer = document.createElement('div');
+    imageContainer.className = 'image-container';
+    imageContainer.style.padding = '5px';
+
+    var imageContainerTitle = document.createElement('h3');
+    imageContainerTitle.style.textAlign = 'center';
+    imageContainerTitle.style.margin = 0;
+
+    var imageContainerImage = document.createElement('img');
+    imageContainerImage.style.display = 'block';
+    imageContainerImage.style.margin = 'auto';    
+    imageContainerImage.style.width = '130px';
+    imageContainerImage.style.height = '130px';
+    imageContainerImage.style.borderRadius = '10px';
+
+    var fragment = document.createDocumentFragment();
+    for(var i = 0, len = items.length; i < len; i += 1){
+        var currtitle = imageContainerTitle.cloneNode(true);
+        currtitle.innerHTML = items[i].title;
+
+        var currImg = imageContainerImage.cloneNode(true);
+        currImg.setAttribute('src', items[i].url);
+        currImg.setAttribute('alt', items[i].title);
+
+        var currContainer = imageContainer.cloneNode(true);
+        currContainer.appendChild(currtitle);
+        currContainer.appendChild(currImg);
+
+        fragment.appendChild(currContainer);
+    }
+
+    rightPanel.appendChild(filterBoxTitle);
+    rightPanel.appendChild(filterBox);
+    rightPanel.appendChild(fragment);
+
+    container.appendChild(leftPanel);
+    container.appendChild(rightPanel);
     
-    imgItem.appendChild(imgItemImg);   
-    imgsList.appendChild(imgItem);
-  }
+    // Events
+    rightPanel.addEventListener('mouseover', function(ev){
+        var target = ev.target;
+        if(target.tagName === 'IMG'){  // || target.tagName === 'H3'
+            target.parentElement.style.background = 'lightgrey';
+            target.style.cursor = 'pointer';          
+        }
+    });
 
-  rightPanel.appendChild(imgsList);
-  
-  /* End of right panel */
+    rightPanel.addEventListener('mouseout', function(ev){
+        var target = ev.target;
+        if(target.tagName === 'IMG'){ // || target.tagName === 'H3'
+            target.parentElement.style.background = '';          
+        }
+    });
 
+    rightPanel.addEventListener('click', function(ev){
+        var target = ev.target;
+        if(target.tagName === 'IMG'){
+            var currImgSrc = target.src;
+            var currImgTitle = target.alt; // previousElementSibling.innerText;            
+            leftPanelTitle.innerText = currImgTitle;
+            leftPanelImage.src = currImgSrc;
+        }
+    });   
 
-  theRow.appendChild(leftPanel);
-  theRow.appendChild(rightPanel);
+    filterBox.addEventListener('input', function(ev){        
+        var inputValue = ev.target.value;
+        var imageContainers = rightPanel.getElementsByClassName('image-container');
+        for(var i = 0, len = imageContainers.length; i < len; i += 1){
+            var currContainer = imageContainers[i];
+            var containerTitle = currContainer.firstElementChild.innerText;
+            if(containerTitle.toLowerCase().includes(inputValue.toLowerCase())){ // or with indexOf(inputValue.toLowerCase()) >= 0
+                currContainer.style.display = '';
+            } else {
+                currContainer.style.display = 'none';
+            }      
+        }
+    });    
 }
