9ca5ff1a9df1c2c55bf2fe6bdb330acacff25cc6
Daniela Popova <d.i.popova@abv.bg>
2017-03-24 14:39:27 +0200

    Update task-3.js

--- JS DOM UI/06. HTML Templates/tasks/task-3.js
+++ JS DOM UI/06. HTML Templates/tasks/task-3.js
@@ -2,7 +2,7 @@ function solve(){
   return function(){
     $.fn.listview = function(data){
         var template = $("#" + this.attr('data-template')).html(),
-            listTemplate = handlebars.compile(template),
+            listTemplate = Handlebars.compile(template),
             i, 
             len;
 
