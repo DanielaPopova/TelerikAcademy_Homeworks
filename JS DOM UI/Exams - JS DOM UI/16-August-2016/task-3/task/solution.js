0f9f71f40798fd3dc6bb9318665662f52d63417b
Daniela Popova <d.i.popova@abv.bg>
2017-03-26 21:46:51 +0300

    added 100/100 solution to Forum task-3

--- JS DOM UI/Exams - JS DOM UI/16-August-2016/task-3/task/solution.js
+++ JS DOM UI/Exams - JS DOM UI/16-August-2016/task-3/task/solution.js
@@ -1,9 +1,41 @@
 function solve() {
 	return function() {
 		var template = [
-			'first line',
-			'second line',
-			'...'
+			'<h1>{{title}}</h1>',
+			'<ul>',
+				'{{#each posts}}',
+				'<li>',
+					'<div class="post">',
+						'<p class="author">',
+							'{{#if author}}',
+								'<a class="user" href="/user/{{author}}">{{author}}</a>',
+							'{{else}}',
+								'<a class="anonymous">Anonymous</a>',
+							'{{/if}}',
+						'</p>',
+						'<pre class="content">{{{text}}}</pre>',
+					'</div>',
+					'<ul>',
+						'{{#each comments}}',
+							'{{#unless deleted}}',
+								'<li>',
+									'<div class="comment">',
+										'<p class="author">',
+											'{{#if author}}',
+												'<a class="user" href="/user/{{author}}">{{author}}</a>',
+											'{{else}}',
+												'<a class="anonymous">Anonymous</a>',
+											'{{/if}}',
+										'</p>',
+										'<pre class="content">{{{text}}}</pre>',
+									'</div>',
+								'</li>',
+							'{{/unless}}',
+						'{{/each}}',
+					'</ul>',
+				'</li>',
+				'{{/each}}',
+			'</ul>'
 		].join('\n');
 
 		return template;
