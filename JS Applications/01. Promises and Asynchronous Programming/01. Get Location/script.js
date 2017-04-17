60ce828bed3924a2b5efb70ded08163e8851e69a
Daniela Popova <d.i.popova@abv.bg>
2017-04-17 15:31:35 +0300

    added getLocation hw

---
+++ JS Applications/01. Promises and Asynchronous Programming/01. Get Location/script.js
@@ -0,0 +1,33 @@
+(function(){
+    let output = document.getElementById("my-location");    
+    let btn = document.getElementById('get-location');
+    btn.addEventListener('click', function () {
+        let getLocation = new Promise(function (resolve, reject) {      
+
+            if (!navigator.geolocation) {
+                output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+                return;
+            }
+
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+
+        output.innerHTML = "<p>Locating…</p>";
+
+        getLocation.then(function (position) {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+
+            output.innerHTML = '<p>Latitude is ' + latitude.toFixed(5) + '° <br>Longitude is ' + longitude.toFixed(5) + '°</p>';
+
+            let img = new Image();
+            img.src = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=13&size=400x400&sensor=false`;
+
+            output.appendChild(img);
+        },
+            function (error) {
+                output.innerHTML = "Unable to retrieve your location";
+            }
+        );
+    });
+}())
