e023579c87bf65fc36ab9b6c8aa2d832f12edc9e
Daniela Popova <d.i.popova@abv.bg>
2017-04-29 16:07:55 +0300

    fixed stylecop warnings

--- High Quality Code Part 2/04. Development Tools/TemplateExample/TemplateExample/Grocery.cs
+++ High Quality Code Part 2/04. Development Tools/TemplateExample/TemplateExample/Grocery.cs
@@ -1,20 +1,12 @@
-﻿
-public class Grocery
+﻿public class Grocery
 {
-
     public string Product0 { get; set; }
-
 
     public string Product1 { get; set; }
 
-
     public string Product2 { get; set; }
-
 
     public string Product3 { get; set; }
 
-
-    public string Product4 { get; set; }
-
-   
+    public string Product4 { get; set; }   
 }
