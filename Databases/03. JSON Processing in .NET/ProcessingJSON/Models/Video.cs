00aa5f71c8916e84cb7b46e34b3884f075e5d52e
Daniela Popova <d.i.popova@abv.bg>
2017-08-11 14:56:15 +0300

    added json processing in .net hw

---
+++ Databases/03. JSON Processing in .NET/ProcessingJSON/Models/Video.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ProcessingJSON.Models
+{
+    public class Video
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("yt:videoId")]
+        public string Id { get; set; }
+
+        [JsonProperty("link")]
+        public Link Link { get; set; }       
+    }
+}
---
+++ Databases/03. JSON Processing in .NET/ProcessingJSON/StartUp.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ProcessingJSON.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ProcessingJSON
+{
+    public class StartUp
+    {
+        private static readonly string Url = @"https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw";
+
+        private static readonly string FilePath = @"../../rssFeed.xml";
+
+        public static void Main()
+        {
+            //Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            DownloadRSS(Url, FilePath);
+
+            var xmlDoc = XDocument.Load(FilePath);
+            string jsonDoc = JsonConvert.SerializeXNode(xmlDoc, Formatting.Indented);
+            //Console.WriteLine(jsonDoc);
+
+            var videos = SelectVideosFromJson(jsonDoc);
+            foreach (var video in videos)
+            {
+                Console.WriteLine("Video title: {0}", video.Title);
+            }
+
+            var html = CreateHtml(videos);
+            string path = string.Format(@"../../TAvideos.html");
+            SaveContentToHtml(path, html);
+        }
+
+        public static void DownloadRSS(string url, string file)
+        {
+            var downloader = new WebClient();
+            //downloader.Encoding = Encoding.UTF8;
+            downloader.DownloadFile(url, file);
+        }
+
+        public static IEnumerable<Video> SelectVideosFromJson(string json)
+        {
+            var jsonObj = JObject.Parse(json);
+            //var videoTitles = from entry in jsonObj["feed"]["entry"]
+            //                  select (string)entry["title"];
+
+            var videos = jsonObj["feed"]["entry"]
+                    .Select(entry => JsonConvert.DeserializeObject<Video>(entry.ToString()));                    
+
+            return videos;
+        }
+
+        public static string CreateHtml(IEnumerable<Video> videos)
+        {
+            StringBuilder html = new StringBuilder();
+            html.Append("<!DOCTYPE html><html><head><title>TelericAcademy Videos</title></head><body>");
+
+            foreach (var video in videos)
+            {
+                html.Append(string.Format("<iframe width=\"420\" height=\"315\" src=\"http://www.youtube.com/embed/{0}\"></iframe>", video.Id));
+            }
+
+            html.Append("</body></html>");
+
+            return html.ToString();
+        }
+
+        public static void SaveContentToHtml(string path, string content)
+        {
+            File.WriteAllText(path, content, Encoding.UTF8);
+        }
+    }
+}
